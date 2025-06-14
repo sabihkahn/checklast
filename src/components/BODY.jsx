25d65a0c5af240ad381d3d1eafbd5c6d5e3aff93
sabihkahn <whyonlyhead@gmail.com>
2025-06-14 13:41:55 +0500

    try

--- src/components/BODY.jsx
+++ src/components/BODY.jsx
@@ -6,7 +6,7 @@ const BODY = () => {
   return (
     <section className="landing">
       <div className="landing-content">
-        <img src={logo} alt="Bracelet Logo" className="landing-logo" />
+        <img src="https://imagekit.io/public/share/vgadgitv7/32fd8d48a2fe804785894aec6c80463e96a795847a846b93701de1d030a3ff8bda49fd309c27e562d03e2d2142376ef00075dd2671a397d09d925073e56090b2658ec0bfac9dbf95fd63bdc9016da4f7" alt="Bracelet Logo" className="landing-logo" />
 
     
       </div>
